4a02ebef63b486106758ce4eb0bf28b84068f04e
Lorenzo <lorenzo.antonelli89@gmail.com>
2021-03-09 09:53:50 +0100

    update con funzion indexOf

--- js/first.js
+++ js/first.js
@@ -8,15 +8,20 @@ console.log(surnameList);
 console.log('Il tuo cognome: ' + surnameUser);
 console.log(surnameList.sort());
 
+// varinte con funzione indexOf
+
+var surnamePosition = surnameList.indexOf(surnameUser) + 1;
+console.log('La posizione del tuo cognome è : ' + surnamePosition);
+
 // variante con ciclo for
 
-for (var i = 0; i < surnameList.length; i++) {
-  var surnamePosition = surnameList[i];
-  if (surnamePosition == surnameUser) {
-    console.log('La posizione del tuo cognome è : ' + (i + 1));
-    break;
-  }
-}
+// for (var i = 0; i < surnameList.length; i++) {
+//   var surnamePosition = surnameList[i];
+//   if (surnamePosition == surnameUser) {
+//     console.log('La posizione del tuo cognome è : ' + (i + 1));
+//     break;
+//   }
+// }
 
 // variante con ciclo while
 
